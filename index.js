d313de6cebd3dee753d9ecbd04d14262c43717fa
eksant <eksant@gmail.com>
2019-10-23 18:16:57 +0800

    Update http or express with gundb

--- index.js
+++ index.js
@@ -1,42 +1,46 @@
-const Gun = require('gun');
-const http = require('http');
-const cors = require('cors');
-const express = require('express');
-const bodyParser = require('body-parser');
+/** express */
+// const express = require('express');
+// const Gun = require('gun');
+// require('gun/sea');
 
-const app = express();
-const server = http.createServer(app);
+// const port = process.env.PORT || 8765;
+// const app = express();
+// app.use(Gun.serve);
+// app.use(express.static(__dirname));
 
-const host = process.env.HOST || 'localhost';
-const port = 7881;
-app.use(
-  bodyParser.json({
-    strict: false,
-  })
-);
-app.use(cors());
-app.use(Gun.serve);
-
-const gun = Gun({ file: 'db', web: server });
-const user = gun.user();
-// user.create('root', 'password', ack => {
-//   console.log('==ack', ack);
-// });
-
-// user.auth('root', 'password', ack => {
-//   if (ack && ack.err) return new Error(ack.err);
-//   // console.log('==ack', ack);
-// });
-
-// function login(ack) {
-//   console.log('login..');
-//   user.auth('bob', 'unsafepassword', write);
-// }
-
-// const users = gun.get('users');
-// console.log('==users', users);
+// var server = app.listen(port);
+// var gun = Gun({ file: 'data', web: server });
 
 // global.Gun = Gun;
 // global.gun = gun;
-server.listen(port);
-console.log(`Database server started in ${host}:${port}`);
+
+// console.log('Server started on port ' + port + ' with /gun');
+
+/** http */
+const fs = require('fs');
+const Gun = require('gun');
+const cluster = require('cluster');
+require('gun/sea');
+
+if (cluster.isMaster) {
+  return (
+    cluster.fork() &&
+    cluster.on('exit', function() {
+      cluster.fork();
+    })
+  );
+}
+
+var config = { port: process.env.PORT || 8765 };
+if (process.env.HTTPS_KEY) {
+  config.key = fs.readFileSync(process.env.HTTPS_KEY);
+  config.cert = fs.readFileSync(process.env.HTTPS_CERT);
+  config.server = require('https').createServer(config, Gun.serve(__dirname));
+} else {
+  config.server = require('http').createServer(Gun.serve(__dirname));
+}
+
+const gun = Gun({ file: 'data', web: config.server.listen(config.port) });
+global.Gun = Gun;
+global.gun = gun;
+console.log('Relay peer started on port ' + config.port + ' with /gun');
